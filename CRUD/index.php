a8711361e5849e82fcaadbace983bf245a4f5799
Airton Siqueira <124066412+SicKera@users.noreply.github.com>
2025-05-15 09:30:19 -0300

    CRUD
    .

---
+++ CRUD/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/style.css">
+    <title>Bem-vindo</title>
+</head>
+<body>
+    <h1>Bem-vindo ao sistema de cadastro</h1>
+    <p>Escolha uma das opções abaixo</p>
+
+    <div class="menu">
+        <a href="back/cadastro.php">Cadastro</a>
+        <a href="back/verificarCadastro.php">Verificar Cadastro</a>
+        <a href="">Atualizar Cadastro</a>
+        <a href="">Verificar Nota</a>
+        <a href="">Atualizar Nota</a>
+    </div>
+</body>
+</html>
